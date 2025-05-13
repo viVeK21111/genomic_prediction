81a1c6f02ed1340d950bd2bb4daed53d002c18c8
vivek vemula <vivekvemula21@gmail.com>
2025-05-13 18:15:07 +0530

    README update

--- ui/app.py
+++ ui/app.py
@@ -1,75 +1,28 @@
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import StandardScaler
-from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score,mean_absolute_error
-import matplotlib.pyplot as plt
-import seaborn as sns
-import pandas as pd
 import numpy as np
-from flask import jsonify,send_file
-import pickle
-import json
-from tensorflow.keras.models import load_model
-from joblib import dump,load
+from joblib import load
+import warnings
+from sklearn.exceptions import ConvergenceWarning
 
+# Suppress specific warnings from scikit-learn
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
 
-df=pd.read_csv("\data\ddata.csv") 
-df=df.drop(columns=['Unnamed: 0'])
-x=df.iloc[:,:-2]
-y=df.iloc[:,-2]
-z=df.iloc[:,-1]
-smote = SMOTE(random_state=42)
-X_resampled, y_resampled = smote.fit_resample(x, y)
-x_train,x_test,y_train,y_test=train_test_split(X_resampled,y_resampled,random_state=42,test_size=0.33) 
- 
-model_r=RandomForestClassifier(n_estimators=7,max_depth=9,random_state=42,criterion="gini",max_features='sqrt',max_leaf_nodes=30) 
-model_r.fit(x_train,y_train)
-s_pred=model_r.predict(x) 
-r_pred=model_r.predict(x_test)
-print(accuracy_score(y_test,r_pred))
-s_pred=list(s_pred)
-df_h=pd.DataFrame(df.iloc[:,:-2]) 
-df_h['sub']=s_pred 
-df_h['sub']=df_h['sub'].astype("category").cat.codes +1  
-df_h['height']=z
-df_h.columns=df_h.columns.astype(str)   
-def Outlier_Treatment(df1,variable):
-    global df_final
-    percentile25=df1[variable].quantile(0.25)  #q1
-    percentile75=df1[variable].quantile(0.75)  #q3
-    iqr=percentile75-percentile25
-    upper_limit=percentile75+1.5*iqr
-    lower_limit=percentile25-1.5*iqr
-    df_final = df1[ (df1[variable]>=lower_limit) & (df1[variable]<=upper_limit) ]
-Outlier_Treatment(df_h,'height')
-xh=df_final.iloc[:,:-1]
-yh=df_final.iloc[:,-1] 
-X_train,X_test,Y_train,Y_test=train_test_split(xh,yh,random_state=42,test_size=0.33)
-from sklearn.preprocessing import MinMaxScaler
-mm=MinMaxScaler()
-mm.fit(X_train)
-X_train=mm.transform(X_train)
-X_test=mm.transform(X_test)
-from sklearn.ensemble import RandomForestRegressor  
-model_rr=RandomForestRegressor(n_estimators=14,max_depth=4,random_state=42,criterion='squared_error')
-from sklearn.ensemble import BaggingRegressor
-bg_model=BaggingRegressor(base_estimator=model_rr,n_estimators=2,random_state=42)
-bg_model.fit(X_train,Y_train)
- 
-lmc=model_r
-lmr=bg_model  
+from flask import Flask,redirect,url_for,render_template,request,send_from_directory
+from flask_compress import Compress
+
+#scaler = load('model/scaler_x.pkl')
+lmc=load('model/model_r.pkl')
+lmr=load('model/model_br.pkl')  
 # lmc=load_model('testc.h5') deep learning model(ann) for classification
 
 
 
 ## flask 
 
-from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
+Compress(app)
 
 @app.route('/')
 def home():
@@ -103,15 +56,10 @@ def func2():
 def empty():
     return render_template('empty.html')
 
-@app.route('/download/<path:filename>')
+@app.route('/download/<filename>')
 def download(filename):
-    file="\\download\\"+filename
-    return send_file(file,as_attachment=True)
+    return send_from_directory('download',filename,as_attachment=True)
 
-@app.route('/downloadt/<path:filename>')
-def downloadt(filename):
-    file="\\download\\"+filename
-    return send_file(file,as_attachment=True) 
 @app.route('/inv')
 def inv():
     return "<h2>Invalid input</h2>"
@@ -166,6 +114,9 @@ def submit():
             enc_dfff['sub']=id
             enc_dfff.columns=enc_dfff.columns.astype(str)
             enc_dfff['sub']=enc_dfff['sub'].astype('category').cat.codes +1 
+            #scaler.fit(enc_dfff)
+            #enc_dfff= scaler.transform(enc_dfff)
+            enc_dfff=enc_dfff.values
             res=lmr.predict(enc_dfff)
             res_df=pd.DataFrame()   
             res_df['sequence']=ss
@@ -178,4 +129,4 @@ def submit():
     else:
         return "invalid request" 
 if __name__=="__main__":
-    app.run() 
+    app.run(threaded=True) 
